641416bc843a7e04acc5af859674daff58bc9b64
carter-allen <carter.allen12@gmail.com>
2017-11-06 19:24:01 -0500

    oddsR

---
+++ R/odds_ratio.R
@@ -0,0 +1,31 @@
+#' A simple function for calculating odds ratios from 2x2 tables
+#'
+#' This function allows for odds ratio calculations.
+#' @param a The exposed + disease + count
+#' @param b The exposed + disease - count
+#' @param c The exposed - disease + count
+#' @param d The exposed - disease - count
+#' @param ci Return a confidence interval instead of point estimate
+#' @param alpha The confidence level of the interval
+#' @keywords odds-ratio
+#' @export
+#' @examples
+#' odds_ratio(30,40,10,50)
+#' odds_ratio(30,40,10,50,ci = TRUE)
+
+odds_ratio <- function(a,b,c,d,alpha = 0.05)
+{
+
+  or = (a*d)/(b*c)
+
+  if(!ci)
+  {
+    return(or)
+  }
+  else
+  {
+    se_lnor = sqrt(1/a+1/b+1/c+1/d)
+    z_crit = qnorm(1-alpha/2)
+    return(exp(pm(log(or),z_crit*se_lnor)))
+  }
+}
---
+++ R/odds_ratio_CMH.R
@@ -0,0 +1,33 @@
+#' A function for calculating Cochran–Mantel–Haenszel odds ratios for across 2 strata
+#'
+#' This function allows for odds ratio calculations across two strata.
+#' @param a A vector of the exposed + disease + count for each stratum
+#' @param b A vector of the exposed + disease - count for each stratum
+#' @param c A vector of the exposed - disease + count for each stratum
+#' @param d A vector of the exposed - disease - count for each stratum
+#' @param ci Return a confidence interval instead of point estimate
+#' @param alpha The confidence level of the interval
+#' @keywords odds-ratio
+#' @export
+#' @examples
+#' odds_ratio(c(30,40),c(20,40),c(50,10),c(10,50))
+
+odds_ratio_CMH <- function(a,b,c,d, ci = FALSE, alpha = 0.05)
+{
+  num = (a[1]*d[1])/sum(a[1],b[1],c[1],d[1]) + (a[2]*d[2])/sum(a[2],b[2],c[2],d[2])
+
+  denom = (b[1]*c[1])/sum(a[1],b[1],c[1],d[1]) + (b[2]*c[2])/sum(a[2],b[2],c[2],d[2])
+
+  if(!ci)
+  {
+    return(num/denom)
+  }
+
+  else
+  {
+    arr <- array(c(a[1],b[1],c[1],d[1],
+                   a[2],b[2],c[2],d[2]),
+                 dim = c(2,2,2))
+    return(mantelhaen.test(arr,conf.level = alpha)$conf.int)
+  }
+}
